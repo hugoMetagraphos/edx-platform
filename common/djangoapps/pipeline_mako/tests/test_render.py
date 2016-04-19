519cb1bb9fc9036828937d3ac263d713782bba83
Matt Drayer <mattdrayer@edx.org>
2016-04-18 21:47:43 -0400

    Revert "Revert "saleem-latif/WL-328: Multi-Site Comprehensive Theming""
    2. Update COMPREHNSIVE_THEME_DIR to COMPREHENSIVE_THEME_DIRS
    3. Update paver commands to support multi theme dirs
    4. Updating template loaders
    5. Add ENABLE_COMPREHENSIVE_THEMING flag to enable or disable theming via settings
    6. Update tests
    7. Add backward compatibility for COMPREHEHNSIVE_THEME_DIR

--- common/djangoapps/pipeline_mako/tests/test_render.py
+++ common/djangoapps/pipeline_mako/tests/test_render.py
@@ -5,9 +5,10 @@ from unittest import skipUnless
 
 from django.conf import settings
 from django.test import TestCase
-from paver.easy import call_task
 
 from pipeline_mako import render_require_js_path_overrides, compressed_css, compressed_js
+
+from openedx.core.djangoapps.theming.tests.test_util import compile_sass
 
 
 class RequireJSPathOverridesTest(TestCase):
@@ -49,7 +50,7 @@ class PipelineRenderTest(TestCase):
         Create static assets once for all pipeline render tests.
         """
         super(PipelineRenderTest, cls).setUpClass()
-        call_task('pavelib.assets.update_assets', args=('lms', '--settings=test'))
+        compile_sass('lms')
 
     @skipUnless(settings.ROOT_URLCONF == 'lms.urls', 'Test only valid in LMS')
     @ddt.data(
